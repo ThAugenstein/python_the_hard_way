e61108d9d44304a5b2135ac5ad1f3ee0f310bd54
Thomas Augenstein <t.augenstein@mac.com>
2024-02-10 20:39:46 +0100

    First 4 exercises done

---
+++ ex1.py
@@ -0,0 +1,6 @@
+print("Hello world!")
+# print("This is fun.")
+# print("Yay! Printing.")
+# print("I'd much rather you 'not'.")
+# print('I "said" do not touch this.')
+# print("My name is 'Tom'")
---
+++ ex3.py
@@ -0,0 +1,37 @@
+# print a string
+print("I will now count my chickens:")
+
+# print the number of hens (30.0)
+print("Hens", 25 + 30 / 6)
+# print the number of roosters (97) 3 % 4 is the remainder of 3 divided by 4
+print("Roosters", 100 - 25 * 3 % 4)
+
+# print a string
+print("Now I will count the eggs:")
+
+# print the number of eggs (6.75)
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+# Ask if 4 is less than -2
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# Print the result (False)
+print(3 + 2 < 5 - 7)
+
+# Print the result of 3 + 2 (5)
+print("What is 3 + 2?", 3 + 2)
+# Print the reuslt of 5 - 7 (-2)
+print("What is 5 - 7?", 5 - 7)
+
+# Print a comment why the comparison above was False
+print("Oh, that's why it's False.")
+
+# Print a string
+print("How about some more.")
+
+# Print the result of 5 > -2 (True)
+print("is it greater?", 5 > -2)
+# Print the result of 5 >= -2 (True)
+print("Ist it greater or equal?", 5 >= -2)
+# Print the result of 5 <= -2 (False)
+print("Is it less or equal?", 5 <= -2)
