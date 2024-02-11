f44b3aaf11833a88ac2e9d538aa10b80f12fa1e2
Thomas Augenstein <t.augenstein@mac.com>
2024-02-11 12:13:34 +0100

    Add ex11.py

---
+++ ex11.py
@@ -0,0 +1,11 @@
+print("How old are you?", end=" ")
+age = input()
+print("How tall are you?", end=" ")
+height = input()
+print("How much do you weight?", end=" ")
+weight = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+name_of_cat = input("What is the name of your cat? ")
+print(f"The name of your cat is '{name_of_cat}'.")
